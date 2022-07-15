498e522fc2eeb76f2d800808070b327948ce8ba1
Duy Tiến <duytienabc123@gmail.com>
2022-07-15 09:42:40 +0700

    Initial commit

---
+++ main.js
@@ -0,0 +1,7 @@
+window.addEventListener("load", () => {
+    const slider = document.querySelector(".slider");
+    const prevBtn = document.querySelector(".slider-prev");
+    const nextBtn = document.querySelector(".slider-next");
+    const productList = document.querySelector(".product__list");
+    const productItems = document.querySelector(".product__item");
+});
